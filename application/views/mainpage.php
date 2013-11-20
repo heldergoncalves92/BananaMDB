16ed04448afdcc4033db34b4b4d93b1ab2d1e0cd
HelderGoncalves92 <heldergoncalves92@live.com.pt>
2013-11-20 12:36:34 +0000

    Testa

--- application/views/mainpage.php
+++ application/views/mainpage.php
@@ -20,7 +20,7 @@
       </div>
     </div>
 
-     <div class="item">
+    <div class="item">
       <img id="carrosel" src="img/Hangover.jpg" >
       <div class="carousel-caption">
        
@@ -33,6 +33,7 @@
        
       </div>
     </div>
+  </div>
 
   <!-- Controls -->
   <a class="left carousel-control" href="#carousel-example-generic" data-slide="prev">
