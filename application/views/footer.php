fede1d6dff951202cd3701ffc41a7dc981d0c4d8
HelderGoncalves92 <heldergoncalves92@live.com.pt>
2013-11-25 14:17:49 +0000

    Like's tem de estar online

--- application/views/footer.php
+++ application/views/footer.php
@@ -12,6 +12,7 @@
      	echo "LOGADO";
 	    ?>
 		
+    <iframe src="//www.facebook.com/plugins/like.php?href=https%3A%2F%2Fwww.facebook.com%2FBananaMDB&amp;width&amp;layout=standard&amp;action=like&amp;show_faces=false&amp;share=false&amp;height=35" scrolling="no" frameborder="0" style="border:none; overflow:hidden; height:35px;" allowTransparency="true"></iframe>
 	
       </footer>
 
